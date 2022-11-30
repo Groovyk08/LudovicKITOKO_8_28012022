55d0a0cf30b9604d5137079a6189ed4b0faaa6b3
Groovyk08 <85116596+Groovyk08@users.noreply.github.com>
2022-11-30 23:06:10 +0100

    new card home

--- src/components/LocationCard/index.jsx
+++ src/components/LocationCard/index.jsx
@@ -1,18 +1,17 @@
 // import PropTypes from 'prop-types'
+import './LocationCard.css'
 
 // Template  d'une carte location
 function LocationCard(props) {
     return (
-        <div>
-            <div className="card">
-                <img
-                    className="imgCard"
-                    src={props.location.cover}
-                    alt={props.location.id}
-                />
-                <div className="cardTitle">
-                    <h1>{props.location.title}</h1>
-                </div>
+        <div className="card">
+            <img
+                className="imgCard"
+                src={props.location.cover}
+                alt={props.location.id}
+            />
+            <div className="cardTitle">
+                <h1>{props.location.title}</h1>
             </div>
         </div>
     )
